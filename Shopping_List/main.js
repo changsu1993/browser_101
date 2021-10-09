54e857e7dc622478a0ada58c7d3607ce2c7241b4
changsu1993 <csdp0106@gmail.com>
2021-10-09 18:19:06 +0900

    Shopping_List event delegation 수정

--- Shopping_List/main.js
+++ Shopping_List/main.js
@@ -23,34 +23,20 @@ function onAdd() {
   input.value = '';
   input.focus();
 }
-
+let id = 0; // UUID
 function createItem(text) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'item__row');
-
-  const item = document.createElement('div');
-  item.setAttribute('class', 'item');
-
-  const span = document.createElement('span');
-  span.setAttribute('class', 'item__name');
-  span.innerText = text;
-
-  const deleteBtn = document.createElement('button');
-  deleteBtn.setAttribute('class', 'item__delete');
-  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-  deleteBtn.addEventListener('click', () => {
-    items.removeChild(itemRow);
-  });
-
-  const itemDivider = document.createElement('div');
-  itemDivider.setAttribute('class', 'item__divider');
-
-  item.appendChild(span);
-  item.appendChild(deleteBtn);
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
-
+  itemRow.setAttribute('data-id', id);
+  itemRow.innerHTML = `
+        <div class="item">
+          <span class="item__name">${text}</span>
+          <button class="item__delete">
+            <i class="fas fa-trash-alt" data-id=${id}></i>
+          </button>
+        </div>
+        <div class="item__devider"></div>`;
+  id++;
   return itemRow;
 }
 
@@ -61,5 +47,13 @@ addBtn.addEventListener('click', () => {
 input.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
     onAdd();
+  }
+});
+
+items.addEventListener('click', (event) => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });
