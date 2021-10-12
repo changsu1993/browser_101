06a89dec89cb4e57fa7c5e3737e1e0bf0712e6b0
changsu1993 <csdp0106@gmail.com>
2021-10-12 20:43:25 +0900

    Carrot_Game 에러 수정 및 보완

--- Carrot_Game/main.js
+++ Carrot_Game/main.js
@@ -11,8 +11,14 @@ const gameBtn = document.querySelector('.game__button');
 const gameTimer = document.querySelector('.game__timer');
 const gameScore = document.querySelector('.game__score');
 const popUp = document.querySelector('.pop-up');
-const popUpRefresh = document.querySelector('pop-up__refresh');
 const popUpText = document.querySelector('.pop-up__message');
+const popUpRefresh = document.querySelector('.pop-up__refresh');
+
+const carrotSound = new Audio('carrot/sound/carrot_pull.mp3');
+const alertSound = new Audio('carrot/sound/alert.wav');
+const bgSound = new Audio('carrot/sound/bg.mp3');
+const bugSound = new Audio('carrot/sound/bug_pull.mp3');
+const winSound = new Audio('carrot/sound/game_win.mp3');
 
 let started = false;
 let score = 0;
@@ -39,6 +45,7 @@ function startGame() {
   showStopButton();
   showTimerAndScore();
   startGameTimer();
+  playSound(bgSound);
 }
 
 function stopGame() {
@@ -46,11 +53,20 @@ function stopGame() {
   stopGameTimer();
   hideGameButton();
   showPopUpWithText('REPLAY?');
+  playSound(alertSound);
+  stopSound(bgSound);
 }
 
 function finishGame(win) {
   started = false;
   hideGameButton();
+  if (win) {
+    playSound(winSound);
+  } else {
+    playSound(bugSound);
+  }
+  stopGameTimer();
+  stopSound(bgSound);
   showPopUpWithText(win ? 'YOU WON 👍🏻' : 'YOU LOST 👎🏻');
 }
 
@@ -58,6 +74,7 @@ function showStopButton() {
   const icon = gameBtn.querySelector('.fas');
   icon.classList.add('fa-stop');
   icon.classList.remove('fa-play');
+  gameBtn.style.visibility = 'visible';
 }
 
 function hideGameButton() {
@@ -102,6 +119,7 @@ function hidePopUp() {
 }
 
 function initGame() {
+  score = 0;
   field.innerHTML = '';
   gameScore.innerText = CARROT_COUNT;
   addItem('carrot', CARROT_COUNT, 'carrot/img/carrot.png');
@@ -117,15 +135,24 @@ function onFieldClick(event) {
     //당근
     target.remove();
     score++;
+    playSound(carrotSound);
     updateScoreBoard();
     if (score === CARROT_COUNT) {
       finishGame(true);
     }
   } else if (target.matches('.bug')) {
     //벌레
-    stopGameTimer();
     finishGame(false);
   }
+}
+
+function playSound(sound) {
+  sound.currentTime = 0;
+  sound.play();
+}
+
+function stopSound(sound) {
+  sound.pause();
 }
 
 function updateScoreBoard() {
